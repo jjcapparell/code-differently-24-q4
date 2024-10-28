c972a8ad3d0acfba716aea9adf5a6c87cbcc9b22
jjcapparell <jjcapparell244@gmail.com>
2024-10-28 12:24:27 +0000

    fix: changed code to fix error

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -8,8 +8,8 @@ export function findPermutationDifference(s: string, t: string): number {
   }
   for (const key of stringS.keys()) {
     if (stringT.has(key)) {
-      const value1 = stringS.get(key)!; // Non-null assertion because we checked with has()
-      const value2 = stringT.get(key)!; // Non-null assertion 
+      const value1 = stringS.get(key) ?? -1; // Non-null assertion because we checked with has()
+      const value2 = stringT.get(key) ?? -1; // Non-null assertion 
       const difference = Math.abs(value1 - value2);
       diffSum += difference;
     }
